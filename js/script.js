1a913e25e1033c6784df6c63549ef789434021d8
mahadi40011 <mahadi40011@gmail.com>
2025-08-28 01:18:46 +0600

    call alert function complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // global variable
-const callHistory = []
+const callHistoryList = []
 
 // function to copy number and paste
 function copy(id, value){
@@ -44,4 +44,42 @@ for(const heartbtn of heartButtons ){
         document.getElementById('heart-count').innerText = heartCount
     })
 }
+
+// fanction to call alert
+function call(id, message, cardTitle, number){
+    const callBtns = document.getElementById(id)
+    callBtns.addEventListener('click', function(){
+        let availableCoin = parseInt(document.getElementById('coin-count').innerText)
+        if(availableCoin >= 20){
+            alert(message)
+            newAvailableCoin = availableCoin - 20
+            document.getElementById('coin-count').innerText = newAvailableCoin
+        } 
+        else{
+            alert('You have not enough coin for the Call')
+        }
+
+        const callCard = {
+            name: cardTitle,
+            service: number,
+            date: new Date().toLocaleTimeString()
+        } 
+
+        callHistoryList.unshift(callCard)
+    })
+}
+
+call('emergency-number-call', "Calling National Emergency Service 999... ", "National Emergency Service", '999')
+call('police-helpline-call', 'Calling Police Helpline Number 999...', 'Police Helpline', '999')
+call('fire-service-call', 'Calling Fire Service Number 999...', 'Fire Service', "999")
+call('ambulance-service_call', "Calling Ambulance Service 1994-999999...", 'Ambulance Service', '1994-999999')
+call("women-child-call", "Calling Women and Child Helpline 109...", "Women and Child Helpline", '109')
+call('anti-corruption-helpline-call', 'Calling Anti Corruption Helpline 106...', 'Anti-Corruption Helpline', '106')
+call('electricity-helpline-call', 'Calling Electricity Helpline 16216...', 'Electricity Helpline', "16216")
+call("brac-helpline-call", 'Calling Brac Helpline 16445...', "Brac Helpline", "16445")
+call('railway-helpline-call', 'Calling Railway Helpline Number 163...', 'Bangladesh Railway Helpline', '163')
+
+
+
+
 
